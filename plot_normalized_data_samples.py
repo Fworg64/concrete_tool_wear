2c9a4cc5205d1ed8ae94ecae8778720d36d3ec40
Austin F. Oltmanns <aoltmanns@mines.edu>
2023-07-16 18:01:27 -0600

    added normalized plots

--- plot_normalized_data_samples.py
+++ plot_normalized_data_samples.py
@@ -29,6 +29,10 @@ from loader import load_audio_files
 from windowizer import Windowizer, window_maker
 from custom_pipeline_elements import SampleScaler, ChannelScaler, FFTMag, WaveletDecomposition
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
 #help words
 shape_options = "hamming,boxcar"
 duration_options = "0 - 10 second duration"
@@ -120,6 +124,7 @@ audio_window = Windowizer(window_maker(args.window_shape, int(args.window_durati
 windowed_audio_data, windowed_audio_labels = audio_window.windowize(raw_audio_data, metadata)
       
 
+wear_list = ["New", "Moderate", "Worn"]
 wear_classes2ints = {"New":0, "Moderate":1, "Worn":2}
 wear_ints2classes = {v: k for k,v in wear_classes2ints.items()}
 
@@ -155,22 +160,94 @@ transformed_data = {label:{} for label in windowed_audio_labels}
 
 for ft1 in freq_transforms1:
   for sc2 in scalings2:
+    for wear in wear_list:
       my_pipeline = Pipeline([ft1, sc2])
 
-      my_pipeline.fit(labelled_data["Worn"])
-      transformed_data["Worn"][sc2[0]] = my_pipeline.transform(labelled_data["Worn"])
-      # Display each freq transform in figure split by category
-      # Fig 1. FFT_Rt normalized distribution
+      my_pipeline.fit(labelled_data[wear])
+      transformed_data[wear][ft1[0]] = my_pipeline.transform(labelled_data[wear])
 
       # Progress UI
       print(".", end='', flush=True)
 
 that_time = time.time()
 print(" Done! Took {0} sec; Saving data...".format(that_time - this_time))
+print(transformed_data)
 
 # Compute distributions
-print(transformed_data)
 # Plot for each wear type and technique
+# Sort data by wear level
+# Find average and variance of each wear level and processing technique
+names = []
+values_list = []
+for ft1 in freq_transforms1:
+  for wear in wear_list:
+    names.append(f"{wear} with {ft1[0]}")
+    values_list.append(np.array(transformed_data[wear][ft1[0]]))
+
+avgs = {}
+devs = {}
+for val, name in zip(values_list, names):
+  avgs[name] = np.mean(val, axis=1)
+  devs[name] = np.std(val, axis=1)
+
+print(names)
+print(avgs)
+
+# Plot distribution vectors for each wear level for frequency
+# Set figure sizes
+fontsize = 18
+plt.rc('font', size=fontsize, family='sans')
+plt.rc('axes', titlesize=fontsize)
+plt.rc('axes', labelsize=fontsize)
+plt.rc('legend', fontsize=fontsize)
+plot_width = 3.6 # pt
+
+for idx,ft1 in enumerate(freq_transforms1):
+  fig, axs = plt.subplots(len(wear_list))
+
+  for index in range(len(wear_list)):
+    # Plot domain
+    print(f"Plotting {names[index]}")
+    domain_vals = np.linspace(0, audio_fs/2, len(avgs[names[index]]))
+    if "Control" in names[index]:
+      domain_vals = np.linspace(0, window_duration, len(avgs[names[index]]))
+      print("DOMAIN CONTROL!")
+
+    namedex = idx * len(wear_list) + index
+    axs[index].fill_between(domain_vals,
+      avgs[names[namedex]] + devs[names[namedex]],
+      avgs[names[namedex]] - devs[names[namedex]],
+      alpha=0.5, color='tab:purple')
+    axs[index].plot(domain_vals, avgs[names[namedex]],
+    color='tab:green', linewidth=plot_width)
+    axs[index].set_title(names[namedex])
+  
+  fig.show()
+    #axs[index][0].set_ylim(-6000, 6000)
+    ##axs[index][0].legend(["Mean", r"$\pm$ 1 Std. Dev."], loc="upper right")
+    #axs[index][0].set_xlabel("Time (s)")
+    #axs[index][0].set_ylabel("Amplitude")
+
+#  # Plot freq domain
+#  axs[index][1].fill_between(freq_vals,
+#    avgs[names[index+3]] + devs[names[index+3]],
+#    avgs[names[index+3]] - devs[names[index+3]],
+#    alpha=0.5, color='tab:purple')
+#  axs[index][1].fill_between([50, 250], 0, 500000, color='tab:red', alpha=0.2)
+#  axs[index][1].fill_between([320, 450], 0, 500000, color='tab:red', alpha=0.2)
+#  axs[index][1].fill_between([750, 850], 0, 500000, color='tab:red', alpha=0.2)
+#  axs[index][1].fill_between([1080, 1110], 0, 500000, color='tab:red', alpha=0.2)
+#  axs[index][1].plot(freq_vals, avgs[names[index+3]],
+#  color='tab:green', linewidth=plot_width)
+#  axs[index][1].set_title(names[index+3])
+#  axs[index][1].set_ylim(0, 3.5e5)
+#  axs[index][1].legend(["Mean", r"$\pm$ 1 Std. Dev."], loc="upper right")
+#  axs[index][1].set_xlabel("Freq. (Hz)")
+#  axs[index][1].set_ylabel("Spectra Magnitude")
+#
+#
+plt.show(block=False)
+input("Press Enter to close...")
 
 
 
